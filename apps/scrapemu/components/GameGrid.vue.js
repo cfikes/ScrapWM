167e515785a962194e88505105f032b6555c1b59
Christopher Fikes <cfikes@fikesmedia.com>
2024-11-08 19:15:17 -0600

    vue updarte

--- apps/scrapemu/components/GameGrid.vue.js
+++ apps/scrapemu/components/GameGrid.vue.js
@@ -1,8 +1,13 @@
 const GameGrid = {
     props: ['games'],
+    computed: {
+      sortedGames() {
+        return [...this.games].sort((a, b) => a.title.localeCompare(b.title));
+      },
+    },
     template: `
       <div class="grid-view">
-        <game-item v-for="game in games" :key="game.title" :game="game"></game-item>
+        <game-item v-for="game in sortedGames" :key="game.title" :game="game"></game-item>
       </div>
     `,
     components: {
